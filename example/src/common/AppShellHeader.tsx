32059b3a63f913210d4fac4fb6bff454f90af65a
Mike Cann <mike.cann@gmail.com>
2025-08-07 13:02:54 +0800

    polishing with a convex logo

--- example/src/common/AppShellHeader.tsx
+++ example/src/common/AppShellHeader.tsx
@@ -1,12 +1,15 @@
-import { Text, Group, Button } from "@mantine/core";
+import { Text, Group, Button, Image } from "@mantine/core";
 import { IconBrandGithub, IconExternalLink } from "@tabler/icons-react";
 
 export function AppShellHeader() {
   return (
     <Group justify="space-between" align="center" h="100%" px="md">
-      <Text size="lg" fw={500} c="white">
-        Convex Aggregate Demo
-      </Text>
+      <Group gap="sm" align="center">
+        <Image src="/convex.svg" alt="Convex Logo" h={32} w={32} />
+        <Text size="lg" fw={500} c="white">
+          Convex Aggregate Demo
+        </Text>
+      </Group>
       <Button
         component="a"
         href="https://github.com/get-convex/aggregate"
