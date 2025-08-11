d0b495a6b78458626823e3e14f4e45d10ff48fdd
Mike Cann <mike.cann@gmail.com>
2025-08-11 09:49:48 +0800

    better variable names

--- example/convex/leaderboard.ts
+++ example/convex/leaderboard.ts
@@ -24,7 +24,7 @@ const aggregateByScore = new TableAggregate<{
   DataModel: DataModel;
   TableName: "leaderboard";
 }>(components.aggregateByScore, {
-  sortKey: (doc) => -doc.score,
+  sortKey: (leaderboardTableDoc) => -leaderboardTableDoc.score,
 });
 
 const aggregateScoreByUser = new TableAggregate<{
