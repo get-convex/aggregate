f8cd40bed1af981dc3d790fe9532b0ca41fd58a6
Reece Yang <7040416+reeceyang@users.noreply.github.com>
2026-06-23 14:51:49 -0700

    bump convex

--- src/component/compare.ts
+++ src/component/compare.ts
@@ -1,4 +1,4 @@
-import type { Value } from "convex/values";
+import { CommitTsPlaceholder, type Value } from "convex/values";
 
 // Returns -1 if k1 < k2
 // Returns 0 if k1 === k2
@@ -80,8 +80,11 @@ function makeComparable(v: Value | undefined): [number, unknown] {
   if (Array.isArray(v)) {
     return [7, v.map(makeComparable)];
   }
+  if (v instanceof CommitTsPlaceholder) {
+    return [8, "CommitTsPlaceholder"];
+  }
   // Otherwise, it's an POJO.
   const keys = Object.keys(v).sort();
   const pojo: Value[] = keys.map((k) => [k, v[k]!]);
-  return [8, pojo.map(makeComparable)];
+  return [9, pojo.map(makeComparable)];
 }
